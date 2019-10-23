fda83d75b3272dcea3828a20c78bfa45fa223964
saiv <saiveeravelli2000@gmail.com>
2019-10-23 15:47:46 -0700

    fixing shit

--- js/security.js
+++ js/security.js
@@ -1,1 +1,15 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', keyredirect)
+function keyredirect(e) {
+  var keycode = e.keyCode;
+
+  if(keycode>=112 && keycode<=123){
+    if(event.keyCode == 123){
+      return false;
+    }else{
+      // do something stupid
+    }
+  }else{
+    window.alert(String.fromCharCode(keycode));
+  }
+}
