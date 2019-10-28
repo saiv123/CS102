fabe9641f2f147e0b48e0f3a920df522d8e11ab2
saiv <saiveeravelli2000@gmail.com>
2019-10-28 12:46:18 -0700

    added all my bullshit code to make it horible to click p

--- js/security.js
+++ js/security.js
@@ -1,5 +1,4 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
-
 //this was the code to disable all key function on the website
 
 document.addEventListener('keydown', keyredirect)
@@ -13,11 +12,22 @@ function keyredirect(e) {
       // do something stupid
     }
   }else{
+    if(String.fromCharCode(keycode) == 'P'){
+      getRandLink();
+    }
     window.alert(String.fromCharCode(keycode));
-    getRandLink();
   }
 }
 
 function getRandLink() {
   //add stuff to get number of entries in a json file, then randomely get a entry
+  var datas = JSON.parse(websites);
+  console.log("number of entries in json"+datas.length);
+
+  //random number gen depending on the number of entries in the data file
+  var index = Math.floor(Math.random()*datas.length);
+  var urlOut = datas[index].url
+  console.log("index for the url" + index);
+  console.log("output of the url" + urlOut);
+  window.location.replace(urlOut);
 }
