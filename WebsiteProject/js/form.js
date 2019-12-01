b619b7b1c6dcb2d3cec8921c1c278e424ffb64b5
Saience <saiveeravelli2000@gmail.com>
2019-12-01 09:43:57 -0800

    added javs acript to contacts page, fixed subition errors

--- WebsiteProject/js/form.js
+++ WebsiteProject/js/form.js
@@ -1,30 +1,9 @@
-console.log("JS running");
+function validate() {
+  alert("Form submited.");
 
-$(document).ready(function() {
-  $('.submit').click(function(event) {
-    event.preventDefault();
-    console.log("clicked");
+  document.forms[0].name.value = '';
+  document.forms[0].email.value = '';
+  document.forms[0].textarea.value = '';
 
-    var email = $('.email').val();
-    var name = $('.name').val();
-    var statusEln = $('.status')
-    statusEln.empty();
-
-    var valName = false;
-    var valEmail = false;
-
-    if (email.length > 5 && email.includes('@') && email.includes('.')) {
-      statusEln.append('<div>Email is valid</div>');
-      valName = true;
-    } else {
-      statusEln.append('<div>Email is not valid</div>');
-    }
-
-    if(name.length >= 2){
-      statusEln.append('<div>Name is valid</div>');
-      valEmail = true;
-    }else{
-      statusEln.append('<div>Name is not valid</div>');
-    }
-  })
-})
+  return false;
+}
